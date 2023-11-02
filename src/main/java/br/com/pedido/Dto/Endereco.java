ef993866a79560dd55213b6af04004452b6bb46a
ElytonPereira <fernandeselyton@gmail.com>
2023-11-02 14:32:35 -0300

    finalizado primeira parte do dto

--- src/main/java/br/com/pedido/Dto/Endereco.java
+++ src/main/java/br/com/pedido/Dto/Endereco.java
@@ -1,5 +1,44 @@
 package br.com.pedido.Dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class Endereco {
 
+	@NotNull(message = "O id é obrigatório")
+	@Positive(message = "O id do endereco deve ser positivo")
+	private Integer id;
+	
+	@Size(max = 45, message = "O nome do endereco não deve conter mais de 45 caracteries")
+	@NotBlank(message = "O nome é obrigatório")
+	private String nome;
+	
+	@Size(max = 45, message = "O cep não deve conter mais de 45 caracteries")
+	@NotBlank(message = "O cep é obrigatório")
+	private String cep;
+	
+	@Size(max = 45, message = "A cidade não deve conter mais de 45 caracteries")
+	@NotBlank(message = "A cidade é obrigatória")
+	private String cidade;
+	
+	@Size(max = 45, message = "O bairro não deve conter mais de 45 caracteries")
+	@NotBlank(message = "O bairro é obrigatório")
+	private String bairro;
+	
+	@Size(max = 45, message = "O estado não deve conter mais de 45 caracteries")
+	@NotBlank(message = "O estado é obrigatório")
+	private String estado;
+	
+	@Size(max = 45, message = "O numero não deve conter mais de 45 caracteries")
+	@NotBlank(message = "O numero é obrigatório")
+	private String numero;
+	
+	private String complemento;
+	
 }
