0a817e3ee2ce446d46ce59ad49a8c77bb60244a3
ÉLYTON FERNANDES PEREIRA <elyton_pereira@estudante.sesisenai.org.br>
2023-11-01 22:25:50 -0300

    incluido cliente

--- src/main/java/br/com/pedido/Dto/Cliente.java
+++ src/main/java/br/com/pedido/Dto/Cliente.java
@@ -1,5 +1,23 @@
 package br.com.pedido.Dto;
 
-public class Cliente {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@AllArgsConstructor
+@Data
+
+public class Cliente {
+	
+	@NotNull(message = "O id é obrigatório")
+	@Positive(message = "O id do cliente deve ser positivo")
+	private Integer id;
+	
+	@Size(max = 100, message = "O nome do cliente não deve conter mais de 100 caracteries")
+	@NotBlank(message = "O nome é obrigatório")
+	private String nome;
+	
 }
