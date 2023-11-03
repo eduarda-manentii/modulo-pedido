f258a32bc638ce2fc2ae11cc8d94f789f435021a
eduarda-manentii <dudamanenti65@gmail.com>
2023-11-03 19:55:07 -0300

    Removido properties.

--- src/main/java/br/com/pedido/entity/Pedido.java
+++ src/main/java/br/com/pedido/entity/Pedido.java
@@ -1,14 +1,5 @@
 package br.com.pedido.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
 public class Pedido {
 
 }
